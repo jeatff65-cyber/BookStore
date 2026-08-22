67f36c725baccc6e929a50af163b5dbba1544cbe
jeatff65 <jeatff65@gmail.com>
2026-08-22 15:38:15 +0700

    Add GitHub Pages deploy setup (user frontend) and Render blueprint for backend

--- BookStore-Frontend_user-main/BookStore-Frontend_user-main/src/index.js
+++ BookStore-Frontend_user-main/BookStore-Frontend_user-main/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
@@ -9,11 +9,11 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
